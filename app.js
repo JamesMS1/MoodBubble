f98e9fc3959cc0752c430c196ffeb5adeec321c1
James Schmidt <james.m.schmidt.1@gmail.com>
2017-08-22 01:41:47 -0700

    adding basic node/express and react pages.

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require( 'express' );
+var path = require('path');
+var app = express();
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var mainPage = require( path.join( __dirname, 'reactComponents', 'mainPage.js' );
+
+app.set('port', process.env.PORT || 3000 );
+
+app.get('/', function( request, responds ) {
+	var html = ReactDOMServer.renderToString(
+			React.createElement( mainPage );
+		
+		responds.send( html );
+})
+
+
+
+
+
+
+
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on host=localhost port=' + app.get('port'));
+});
